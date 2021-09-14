8102eb70642ed0cc8f84796b6e8dea6aec86fedb
TecuAndreea <tecu_andreea@yahoo.com>
2021-09-14 12:10:18 +0300

    Project creation

---
+++ TaraniShop/TaraniShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TaraniShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
